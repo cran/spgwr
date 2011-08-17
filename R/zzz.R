49b6ca7c9cd323b347b114b0c200db88c79419da
Roger Bivand <Roger.Bivand@nhh.no>
2011-08-17 00:00:00 +0000

    version 0.6-13

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
-require(sp)
-.First.lib <- function(lib, pkg) {
-	library.dynam("spgwr", pkg, lib)
+.onLoad <- function(lib, pkg) {
+#	library.dynam("spgwr", pkg, lib)
 	packageStartupMessage(paste(
-	"NOTE: default kernel and CV criteria changed", 
-	"see help pages for details\n", sep="\n"), appendLF = FALSE)
+            "NOTE: This package does not constitute approval",
+            "of GWR as a method of spatial analysis\n", sep="\n"),
+            appendLF = FALSE)
 }
--- data/georgia.R
+++
@@ -1,2 +0,0 @@
-require(maptools)
-gSRDF <- readShapePoly(system.file("shapes/georgia.shp", package="spgwr")[1], proj4string=CRS("+proj=longlat +datum=NAD27"))
