610c6c3bfbd84f8d107392f0c16127060c3bb80f
Roger Bivand <Roger.Bivand@nhh.no>
2017-10-29 19:03:59 +0000

    version 0.6-32

--- inst/doc/GWR.R
+++ inst/doc/GWR.R
@@ -57,57 +57,54 @@ options("warn"=-1)
 
 
 ###################################################
-### code chunk number 8: GWR.Rnw:107-113
+### code chunk number 8: GWR.Rnw:107-110 (eval = FALSE)
 ###################################################
-library(maptools)
-library(spdep)
-owd <- getwd()
-setwd(system.file("etc/shapes", package="spdep"))
-NY8 <- readShapeSpatial("NY8_utm18")
-setwd(owd)
+## if (require(rgdal, quietly=TRUE)) {
+##   NY8 <- readOGR(system.file("shapes/NY8_utm18.shp", package="spData"))
+## }
 
 
 ###################################################
-### code chunk number 9: GWR.Rnw:119-121
+### code chunk number 9: GWR.Rnw:116-118
 ###################################################
 load(system.file("backstore/nyGWR.RData", package="spgwr"))
 #save(bwG, gwrG, gbwG, ggwrG, file="nyGWR.RData")
 
 
 ###################################################
-### code chunk number 10: GWR.Rnw:124-125
+### code chunk number 10: GWR.Rnw:121-122
 ###################################################
 library(spgwr)
 
 
 ###################################################
-### code chunk number 11: GWR.Rnw:127-129 (eval = FALSE)
+### code chunk number 11: GWR.Rnw:124-126 (eval = FALSE)
 ###################################################
 ## bwG <- gwr.sel(Z~PEXPOSURE+PCTAGE65P+PCTOWNHOME, data=NY8, gweight=gwr.Gauss, verbose=FALSE)
 ## gwrG <- gwr(Z~PEXPOSURE+PCTAGE65P+PCTOWNHOME, data=NY8, bandwidth=bwG, gweight=gwr.Gauss, hatmatrix=TRUE)
 
 
 ###################################################
-### code chunk number 12: GWR.Rnw:131-132
+### code chunk number 12: GWR.Rnw:128-129
 ###################################################
 gwrG
 
 
 ###################################################
-### code chunk number 13: GWR.Rnw:168-170 (eval = FALSE)
+### code chunk number 13: GWR.Rnw:165-167 (eval = FALSE)
 ###################################################
 ## gbwG <- ggwr.sel(Cases~PEXPOSURE+PCTAGE65P+PCTOWNHOME+offset(log(POP8)), data=NY8, family="poisson", gweight=gwr.Gauss, verbose=FALSE)
 ## ggwrG <- ggwr(Cases~PEXPOSURE+PCTAGE65P+PCTOWNHOME+offset(log(POP8)), data=NY8, family="poisson", bandwidth=gbwG, gweight=gwr.Gauss)
 
 
 ###################################################
-### code chunk number 14: GWR.Rnw:172-173 (eval = FALSE)
+### code chunk number 14: GWR.Rnw:169-170 (eval = FALSE)
 ###################################################
 ## ggwrG
 
 
 ###################################################
-### code chunk number 15: GWR.Rnw:175-179
+### code chunk number 15: GWR.Rnw:172-176
 ###################################################
 xx <- capture.output(print(ggwrG))
 cat(xx[1], sep="\n")
@@ -116,7 +113,7 @@ cat(xx[4:11], sep="\n")
 
 
 ###################################################
-### code chunk number 16: GWR.Rnw:194-197
+### code chunk number 16: GWR.Rnw:191-194
 ###################################################
 .PngNo <- .PngNo + 1; file <- paste("Fig-bitmap-", .PngNo, ".pdf", sep="")
 pdf(file=file, width = 6.5, height = 5, pointsize = 12, bg = "white")
@@ -128,7 +125,7 @@ cat("\\includegraphics[width=0.95\\textwidth]{", file, "}\n\n", sep="")
 
 
 ###################################################
-### code chunk number 17: GWR.Rnw:202-205
+### code chunk number 17: GWR.Rnw:199-202
 ###################################################
 .PngNo <- .PngNo + 1; file <- paste("Fig-bitmap-", .PngNo, ".pdf", sep="")
 pdf(file=file, width = 6.5, height = 5, pointsize = 12, bg = "white")
@@ -140,7 +137,7 @@ cat("\\includegraphics[width=0.95\\textwidth]{", file, "}\n\n", sep="")
 
 
 ###################################################
-### code chunk number 18: GWR.Rnw:213-215
+### code chunk number 18: GWR.Rnw:210-212
 ###################################################
 options("width"=owidth)
 options("warn"=ow)
