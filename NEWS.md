b6d666def20b04d044125abd829bb25690695637
Roger Bivand <Roger.Bivand@nhh.no>
2024-06-10 13:20:02 +0000

    version 0.6-37

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,8 @@
-# Version 0.6-36 (development)
+# Version 0.6-37 (development)
+
+* #5 use GPKG not ESRI Shapefile from `spData`
+
+# Version 0.6-36 (2023-06-14)
 
 * start github repo
 
--- inst/doc/GWR.R
+++ inst/doc/GWR.R
@@ -1,29 +1,36 @@
-## ---- echo=FALSE, results="hide"----------------------------------------------
+## ----echo=FALSE, results="hide"-----------------------------------------------
 load(system.file("backstore/nyGWR.RData", package="spgwr"))
 
 ## -----------------------------------------------------------------------------
 library(spgwr)
 
-## ---- eval=FALSE--------------------------------------------------------------
-#  NY8 <- as(st_read(system.file("shapes/NY8_utm18.shp", package="spData")), "Spatial")
+## ----eval=FALSE---------------------------------------------------------------
+#  if (packageVersion("spData") >= "2.3.2") {
+#      NY8a <- sf::st_read(system.file("shapes/NY8_utm18.gpkg", package="spData"))
+#  } else {
+#      NY8a <- sf::st_read(system.file("shapes/NY8_bna_utm18.gpkg", package="spData"))
+#      sf::st_crs(NY8a) <- "EPSG:32618"
+#      NY8a$Cases <- NY8a$TRACTCAS
+#  }
+#  NY8 <- as(NY8a, "Spatial")
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  bwG <- gwr.sel(Z~PEXPOSURE+PCTAGE65P+PCTOWNHOME, data=NY8, gweight=gwr.Gauss, verbose=FALSE)
 #  gwrG <- gwr(Z~PEXPOSURE+PCTAGE65P+PCTOWNHOME, data=NY8, bandwidth=bwG, gweight=gwr.Gauss, hatmatrix=TRUE)
 
 ## -----------------------------------------------------------------------------
 gwrG
 
-## ---- eval=FALSE--------------------------------------------------------------
+## ----eval=FALSE---------------------------------------------------------------
 #  gbwG <- ggwr.sel(Cases~PEXPOSURE+PCTAGE65P+PCTOWNHOME+offset(log(POP8)), data=NY8, family="poisson", gweight=gwr.Gauss, verbose=FALSE)
 #  ggwrG <- ggwr(Cases~PEXPOSURE+PCTAGE65P+PCTOWNHOME+offset(log(POP8)), data=NY8, family="poisson", bandwidth=gbwG, gweight=gwr.Gauss)
 
 ## -----------------------------------------------------------------------------
 ggwrG
 
-## ---- fig.caption="GWR local coefficient estimates for the exposure to TCE site covariate"----
+## ----fig.caption="GWR local coefficient estimates for the exposure to TCE site covariate"----
 spplot(ggwrG$SDF, "PEXPOSURE", col.regions=grey.colors(7, 0.95, 0.55, 2.2), cuts=6)
 
-## ---- fig.caption="plots of GWR local coefficient estimates showing the effects of GWR collinearity forcing"----
+## ----fig.caption="plots of GWR local coefficient estimates showing the effects of GWR collinearity forcing"----
 pairs(as(ggwrG$SDF, "data.frame")[,2:5])
 
